3a9f6f0f1fa5d3bec6fec4ccae2e258ca6b0db0a
loupnunux <loup.nunux@gmail.com>
2017-10-23 13:18:06 +0200

    correction

--- app/controllers/saints_controller.rb
+++ app/controllers/saints_controller.rb
@@ -1,5 +1,6 @@
 class SaintsController < ApplicationController
   before_action :set_saint, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /saints
   # GET /saints.json
